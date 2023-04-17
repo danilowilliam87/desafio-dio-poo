f2ca4c02b17880a9e00572cc8ff75a77761cb103
danilo <danilowilliam87@outlook.com>
2023-04-17 16:27:33 -0300

    criacao das classes de modelo - Dev e Bootcamp

---
+++ src/main/java/org/labs/model/Bootcamp.java
@@ -0,0 +1,79 @@
+package org.labs.model;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
+
+public class Bootcamp {
+
+    private String nome;
+    private String descricao;
+    private LocalDate dataInicio = LocalDate.now();
+    private LocalDate dataFinal = dataInicio.plusDays(45);
+    private Set<Conteudo> conteudos = new LinkedHashSet<>();
+    private Set<Dev> devsIncritos = new HashSet<>();
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public LocalDate getDataInicio() {
+        return dataInicio;
+    }
+
+    public void setDataInicio(LocalDate dataInicio) {
+        this.dataInicio = dataInicio;
+    }
+
+    public LocalDate getDataFinal() {
+        return dataFinal;
+    }
+
+    public void setDataFinal(LocalDate dataFinal) {
+        this.dataFinal = dataFinal;
+    }
+
+    public Set<Conteudo> getConteudos() {
+        return conteudos;
+    }
+
+    public void setConteudos(Set<Conteudo> conteudos) {
+        this.conteudos = conteudos;
+    }
+
+    public Set<Dev> getDevsIncritos() {
+        return devsIncritos;
+    }
+
+    public void setDevsIncritos(Set<Dev> devsIncritos) {
+        this.devsIncritos = devsIncritos;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Bootcamp bootcamp = (Bootcamp) o;
+        return Objects.equals(nome, bootcamp.nome) && Objects.equals(descricao, bootcamp.descricao) && Objects.equals(dataInicio, bootcamp.dataInicio) && Objects.equals(dataFinal, bootcamp.dataFinal) && Objects.equals(conteudos, bootcamp.conteudos) && Objects.equals(devsIncritos, bootcamp.devsIncritos);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, descricao, dataInicio, dataFinal, conteudos, devsIncritos);
+    }
+}
---
+++ src/main/java/org/labs/model/Dev.java
@@ -0,0 +1,59 @@
+package org.labs.model;
+
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
+
+public class Dev {
+
+    private String nome;
+    private Set<Conteudo> conteudosInscritos = new LinkedHashSet<Conteudo>();
+    private Set<Conteudo> conteudosFinalizados = new LinkedHashSet<Conteudo>();
+
+    public void increverBootcamp(Bootcamp bootcamp){
+
+    }
+
+    public void progredirBootcamp(Bootcamp bootcamp){
+
+    }
+
+    public void calcularTotalXP(){}
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Set<Conteudo> getConteudosInscritos() {
+        return conteudosInscritos;
+    }
+
+    public void setConteudosInscritos(Set<Conteudo> conteudosInscritos) {
+        this.conteudosInscritos = conteudosInscritos;
+    }
+
+    public Set<Conteudo> getConteudosFinalizados() {
+        return conteudosFinalizados;
+    }
+
+    public void setConteudosFinalizados(Set<Conteudo> conteudosFinalizados) {
+        this.conteudosFinalizados = conteudosFinalizados;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Dev dev = (Dev) o;
+        return Objects.equals(nome, dev.nome) && Objects.equals(conteudosInscritos, dev.conteudosInscritos) && Objects.equals(conteudosFinalizados, dev.conteudosFinalizados);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, conteudosInscritos, conteudosFinalizados);
+    }
+}
